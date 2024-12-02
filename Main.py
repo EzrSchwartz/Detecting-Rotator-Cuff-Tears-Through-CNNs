6bcb00d3fc81aacd9330df958ef042bce71630f8
Ezra Schwartz <exoticjoe841@gmail.com>
2024-12-02 14:14:46 -0500

    Making .ipynb

--- Main.py
+++ Main.py
@@ -34,70 +34,7 @@ if __name__ == "__main__":
 
 
 
-class Bran2d2CNN(nn.Module):
-    def __init__(self, TransferModel):
-        super(Bran2d2CNN, self).__init__()
-        self.TransferModel = TransferModel
-        
-        self.fc1 = nn.Linear(in_features=TransferModel.fc_input_size, out_features=16)
-        self.fc2 = nn.Linear(in_features=16, out_features=4)
-    def forward(self, x):
-        x = self.TransferModel(x)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
-        return x
-
 
 if __name__ == "__main__":
-    # TransferModel= Convolutional_autoencoder()
-    # model = ShoulderClassificationmodel(TransferModel)
-    # model2 = RandomInitModelReplica()
-    # print("model", model)
-    # print("model2", model2)
-
-    # Shoulderdataset = R"Path to shoulder data directory"
-    # TransferDataset = R"Path to transfer data directory"
-
-    # Shoulders = CustomDataset(Shoulderdataset)
-    # Transfer = CustomDataset(TransferDataset)
-
-    
-    # Shoulders = Random3DDataset(num_samples=1000)
-    # Transfer = Random3DDataset(num_samples=1000)
-
-    # print("Datasets Made")
-
-    # ShoulderDataLoader = DataLoader(Shoulders, batch_size=1, shuffle=True)
-    # TransferDataLoader = DataLoader(Transfer, batch_size=1, shuffle=True)
-
-    # for batch_idx, (data_input, labels) in enumerate(ShoulderDataLoader):
-    #     continue
-
-    # for batch_idx, (data_input) in enumerate(TransferDataLoader):
-    #     continue
-    # print("Transfer Data Dataloader length:", len(TransferDataLoader))
-
-    # print("Shoulder Data Dataloader Length:", len(ShoulderDataLoader))
-
-    # for batch_idx, (data_input, labels) in enumerate(ShoulderDataLoader):
-    #     print(f"Batch {batch_idx}, Data shape: {data_input.shape}, Labels: {labels}")
-
     transferModel(1, random(1000), random(1000))
-                  
-
-#currently set up for randominit model, can change to transfer model if you replace "model2" with "model" in the for loops below
-    # for epoch in range(numEpoch):
-    #     for batch_idx, (data_input) in enumerate(TransferDataLoader):
-    #         optimizer = torch.optim.Adam(model2.parameters(), lr=0.001)
-    #         optimizer.step()
-
-    #         for batch_idx, (data_input, labels) in enumerate(ShoulderDataLoader):
-    #             outputs = model2(data_input)
-    #             loss = F.cross_entropy(outputs, labels)
-    #             print("Epoch:", epoch, "Loss:", loss.item())
-    #             torch.save(model2.state_dict(), f'Path to where we save the models')
-    #         loss = F.cross_entropy(outputs, labels)
-
-
-    #     torch.save(model.state_dict(), f'Path to where we save the different versions of the transfer learning models')
+                
