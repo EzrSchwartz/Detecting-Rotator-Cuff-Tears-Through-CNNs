4e75f64b8c32435a76404ac96190c21d881a2145
EzrSchwartz <ezra.n.schwartz@gmail.com>
2024-12-04 11:40:54 -0500

    Update imports in main

--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ from PIL import Image
 import tqdm
 from tqdm import tqdm
 from Synthetic3dDataset import Random3DDataset
-from Clymer import randintModel, transferModelfrom Clymer import randintModel, transferModel
+from Clymer import randintModel, transferModel
 from Datasets import shoulders, transfer, random
 
 
