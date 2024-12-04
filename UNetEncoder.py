0288567e44bc39769d17066322f9e65e9f87e4d8
EzrSchwartz <ezra.n.schwartz@gmail.com>
2024-12-04 17:38:46 -0500

    Tiny Change

--- UNetEncoder.py
+++ UNetEncoder.py
@@ -84,6 +84,4 @@ class UNet3D(nn.Module):
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = UNet3D().to(device)
 input_data = torch.rand((1, 1, 128, 128, 128), device=device)
-print(f"Output shape: {output.shape}")
-
-
+print(f"Output shape: {model.shape}")
