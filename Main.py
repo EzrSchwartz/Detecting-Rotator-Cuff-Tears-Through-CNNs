4d931388eadf7253122b3c2d9aa0f0619553375a
EzrSchwartz <ezra.n.schwartz@gmail.com>
2024-12-09 11:10:36 -0500

    Working on UNet and making outputs ENC2 and DEC2 match

--- Main.py
+++ Main.py
@@ -15,18 +15,18 @@ from UNetEncoder import UNet
 
 
 if __name__ == "__main__":
-    dataset = Random3DDataset()
+    # dataset = Random3DDataset()
 
-    dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
+    # dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
     
-    for batch_idx, (data, labels) in enumerate(dataloader):
-        print(f"Batch {batch_idx}")
-        print(f"Data shape: {data.shape}")  # Should be (batch_size, 1, depth, height, width)
-        print(f"Labels: {labels}")
+    # for batch_idx, (data, labels) in enumerate(dataloader):
+    #     print(f"Batch {batch_idx}")
+    #     print(f"Data shape: {data.shape}")  # Should be (batch_size, 1, depth, height, width)
+    #     print(f"Labels: {labels}")
         
-        if batch_idx == 0:  # print first batch
-            break
+    #     if batch_idx == 0:  # print first batch
+    #         break
         
-    transferModel(1, random(1000), random(1000))
-    UNet()
-                
+    # transferModel(1, random(1000), random(1000))
+    UNet(1,random(1000))
+             
