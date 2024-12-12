38ceb49b7d1f2da83527458c715b83fa5c3de502
EzrSchwartz <ezra.n.schwartz@gmail.com>
2024-12-12 14:36:49 -0500

    Got access to images
    implement image augment then to tensor in next commit

--- ImageAug.py
+++ ImageAug.py
@@ -2,9 +2,25 @@ import imgaug.augmenters as iaa
 import tqdm 
 from tqdm import tqdm
 
+import os as os
+from PIL import Image
+
+def extractImages(rootDirectory):
+    for root,dirs,files in os.walk(rootDirectory):
+        for dir in dirs:
+            print(dir)
+            for image in os.listdir(os.path.join(rootDirectory,dir)):
+                if image.endswith(".jpg") or image.endswith(".png"):
+                    print(image)
+
+                    continue
+                
 
 seq = iaa.Sequential([
-    iaa.rotate(-180,180),
-    iaa.gaussian_noise(severity(2))
+    # iaa.Rotate(,180),
+    # iaa.gaussian_noise(severity(2))
     
 ])
+
+extractImages("D:\ShoulderTears\ShoulderTest")
+
--- Main.py
+++ Main.py
@@ -29,4 +29,3 @@ if __name__ == "__main__":
         
     # transferModel(1, random(1000), random(1000))
     UNet(1,random(10000))
-             
